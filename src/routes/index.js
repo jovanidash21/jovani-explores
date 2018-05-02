174c7a366e025573edcdd543083b37181d67d0b0
Jovani Cadorna Warguez <jwardash21@gmail.com>
2018-05-02 10:14:05 +0800

    Delete Work Container

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Switch } from 'react-router';
 import Layout from '../containers/Layout';
 import Home from '../containers/Home';
-import Work from '../containers/Work';
-import WorkSingle from '../containers/Work/Single';
 import Blog from '../containers/Blog';
 import BlogSingle from '../containers/Blog/Single';
 import About from '../containers/About';
@@ -14,8 +12,6 @@ const routes = (
   <div>
     <Switch>
       <Layout exact path="/" component={Home} />
-      <Layout exact path="/work" component={Work} />
-      <Layout exact path="/work/:slug" component={WorkSingle} />
       <Layout exact path="/blog" component={Blog} />
       <Layout exact path="/blog/:slug" component={BlogSingle} />
       <Layout exact path="/about" component={About} />
