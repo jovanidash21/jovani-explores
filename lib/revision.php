7601f24b1284ccb8a261c0ecc6c04ad1016a1929
Jovani Cadorna Warguez <jwardash21@gmail.com>
2018-05-06 20:44:37 +0800

    Add Asset Revisioning Gulp Task

---
+++ lib/revision.php
@@ -0,0 +1,17 @@
+<?php
+
+function revision() {
+
+  // Styles
+  wp_enqueue_style( 'vendor-css', get_template_directory_uri() . '/dist/styles/vendor.css', false, '66197b5102286dcf611245947fb295d9');
+  wp_enqueue_style( 'main-css', get_template_directory_uri() . '/dist/styles/main.css', false, '2e307dedcbc47722079aadc3c8ebd449');
+
+  // Scripts
+  wp_enqueue_script( 'vendor-js', get_template_directory_uri() . '/dist/scripts/vendor.js', array(), 'c5f3777e404b0ed86469e645edd56cb5', true );
+  wp_enqueue_script( 'main-js', get_template_directory_uri() . '/dist/scripts/main.js', array(), '501f2ba53684cae21a53cb26c8848f2c', true );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'revision', 101 );
+
+?>
